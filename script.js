2cc892d292031d5ffb395c20357c20596b25f654
Justin <reasonablebrews@gmail.com>
2024-08-13 20:50:40 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('random-prospect-btn').addEventListener('click', function() {
+    alert('Random draft prospect generation is not yet implemented.');
+});
+
+document.getElementById('other-content-btn').addEventListener('click', function() {
+    alert('Other content functionality is not yet implemented.');
+});
